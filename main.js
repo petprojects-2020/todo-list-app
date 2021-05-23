ab17fa9f66bb23b3b21a4d078f032762288f765b
petprojects-2020 <for.pet.projects@gmail.com>
2021-05-23 14:53:23 +0300

    refactoring_func_done

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const input = document.getElementById('task__title');
 const form = document.getElementById('task__form');
 const tasksList = document.getElementById('tasks__list');
-const deleteCompleted = document.getElementById('delete__completed');
 const btnHolder = document.getElementById('button__holder');
-let counter = document.getElementById('counter');
-let counterText = document.getElementById('counter__text');
+const counter = document.getElementById('counter');
+const counterText = document.getElementById('counter__text');
+const completedTasks = document.getElementById('filter__completed');
+const activeTasks = document.getElementById('filter__active');
+const allTasks = document.getElementById('filter__all');
+const deleteCompleted = document.getElementById('delete__completed');
+
 counter.innerHTML = 0;
-
 const tasksArray = [];
-
 class Task {
   id = Math.floor(Math.random() * 10000);
   isCompleted = false;
@@ -16,7 +18,76 @@ class Task {
     this.title = title;
   }
 }
-// <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+
+const displayButtons = () => {
+  if(tasksArray.length > 0) {
+    btnHolder.classList.remove('d-none');
+    btnHolder.classList.add('d-block', 'col-7');
+  } else {
+    btnHolder.classList.add('d-none');
+  }
+}
+
+const deleteCompletedBtn = () => {
+  const filteredArray = tasksArray.filter(task => task.isCompleted === true);
+  if(filteredArray.length === 0) {
+    deleteCompleted.classList.add('d-none');
+  } else {
+    deleteCompleted.classList.remove('d-none');
+    deleteCompleted.classList.add('d-block','btn','btn-outline-secondary','btn-sm');
+  }
+}
+
+const displayTasks = (array) => {
+  const filteredArray = tasksArray.filter(task => task.isCompleted === false);
+  counter.innerHTML = filteredArray.length;
+  filteredArray.length === 1 ? counterText.innerHTML = 'item left' : counterText.innerHTML = 'items left';
+  tasksList.innerHTML = null;
+  array.forEach(task => {
+    if(task.isCompleted === true) {
+      const div = document.createElement('div');
+      const input = document.createElement('input');
+      const span = document.createElement('span');
+      const btnDelete = document.createElement('button');
+      btnDelete.appendChild(document.createTextNode('delete'));
+      input.type = 'checkbox';
+      input.checked = true; 
+
+      input.classList.add('form-check-input');
+      span.classList.add('task__title', 'inline-block', 'mx-3', 'completed');
+      div.classList.add('border', 'rounded', 'd-flex', 'justify-content-start', 'p-2', 'task__container');
+      btnDelete.classList.add('btn', 'btn-sm', 'btn-danger', 'ms-auto');
+    
+      span.appendChild(document.createTextNode(task.title))
+      div.appendChild(input);
+      div.appendChild(span);
+      div.appendChild(btnDelete);
+      div.id = task.id;
+      tasksList.appendChild(div);
+      
+    } else {
+      const div = document.createElement('div');
+      const input = document.createElement('input');
+      const span = document.createElement('span');
+      const btnDelete = document.createElement('button');
+      btnDelete.appendChild(document.createTextNode('delete'));
+      input.type = 'checkbox';
+
+      input.classList.add('form-check-input');
+      span.classList.add('task__title', 'inline-block', 'mx-3');
+      div.classList.add('border', 'rounded', 'd-flex', 'justify-content-start', 'p-2', 'task__container');
+      btnDelete.classList.add('btn', 'btn-sm', 'btn-danger', 'ms-auto');
+
+      span.appendChild(document.createTextNode(task.title))
+      div.appendChild(input);
+      div.appendChild(span);
+      div.appendChild(btnDelete);
+      div.id = task.id;
+      tasksList.appendChild(div);
+    }
+  });
+}
+
 // Create task
 
 form.onsubmit = (event) => {
@@ -25,80 +96,8 @@ form.onsubmit = (event) => {
   const addedTask = new Task(taskTitle);
   tasksArray.push(addedTask);
   input.value = null;
-  console.log(tasksArray);
-  displayTasks();
-}
-
-// Display task
-
-const displayTasks = () => {
-  counter.innerHTML = tasksArray.length;
-  if(tasksArray.length === 1) {
-    counterText.innerHTML = 'item left';
-  } else {
-    counterText.innerHTML = 'items left';
-  }
-  counter.innerHTML = tasksArray.length;
-  tasksList.innerHTML = null;
-  if(tasksArray.length > 0) {
-    btnHolder.classList.remove('d-none');
-    btnHolder.classList.add('d-block', 'col-7');
-  } else {
-    btnHolder.classList.add('d-none');
-  }
-  tasksArray.forEach(task => {
-    if(task.isCompleted === true) {
-      const div = document.createElement('div');
-      const input = document.createElement('input')
-      const span = document.createElement('span');
-      const btnDelete = document.createElement('button');
-      const btnComplete = document.createElement('button');
-      btnDelete.appendChild(document.createTextNode('delete'));
-      btnComplete.appendChild(document.createTextNode('complete'));
-      input.type = 'checkbox';
-      input.classList.add('form-check-input');
-      input.checked = true;
-      span.classList.add('task__title', 'inline-block', 'mx-3');
-      span.classList.add('completed');
-  
-      div.classList.add('border', 'rounded', 'd-flex', 'justify-content-start', 'p-2');
-      btnComplete.classList.add('btn', 'btn-sm', 'btn-success');
-      btnDelete.classList.add('btn', 'btn-sm', 'btn-danger');
-      span.appendChild(document.createTextNode(task.title))
-      div.appendChild(input);
-      div.classList.add('task__container')
-      div.appendChild(span);
-      div.appendChild(btnComplete);
-      div.appendChild(btnDelete);
-      div.id = task.id;
-
-
-      tasksList.appendChild(div);
-    } else {
-      const div = document.createElement('div');
-      const input = document.createElement('input')
-      const span = document.createElement('span');
-      const btnDelete = document.createElement('button');
-      const btnComplete = document.createElement('button');
-      btnDelete.appendChild(document.createTextNode('delete'));
-      btnComplete.appendChild(document.createTextNode('complete'));
-      input.type = 'checkbox';
-      input.classList.add('form-check-input', 'pr-4');
-      span.classList.add('task__title', 'inline-block', 'mx-3');
-  
-      div.classList.add('border', 'rounded', 'd-flex', 'justify-content-start', 'p-2');
-      btnComplete.classList.add('btn', 'btn-sm', 'btn-success');
-      btnDelete.classList.add('btn', 'btn-sm', 'btn-danger', 'ms-auto');
-      span.appendChild(document.createTextNode(task.title))
-      div.appendChild(input);
-      div.classList.add('task__container')
-      div.appendChild(span);
-      div.appendChild(btnComplete);
-      div.appendChild(btnDelete);
-      div.id = task.id;
-      tasksList.appendChild(div);
-    }
-  });
+  displayTasks(tasksArray);
+  displayButtons();
 }
 
 // Delete and Complete task
@@ -109,24 +108,19 @@ const deleteTask = (event) => {
     tasksArray.splice(index, 1);
     tasksList.removeChild(event.target.parentNode);
     counter.innerHTML = tasksArray.length;
-    displayTasks();
-  } else if(event.target.innerHTML==="complete") {
-    const index = tasksArray.findIndex(el => +event.target.parentNode.id === el.id);
-    tasksArray[index].isCompleted = true;
-    deleteCompleted.classList.remove('d-none');
-    deleteCompleted.classList.add('d-block','btn','btn-outline-secondary','btn-sm');
-    displayTasks();
+    displayTasks(tasksArray);
   } else if(event.target.type === 'checkbox') {
     if(event.target.checked) {
       const index = tasksArray.findIndex(el => +event.target.parentNode.id === el.id);
       tasksArray[index].isCompleted = true;
-      deleteCompleted.classList.remove('d-none');
-      deleteCompleted.classList.add('d-block','btn','btn-outline-secondary','btn-sm');
-      displayTasks();
+      deleteCompletedBtn();
+      displayTasks(tasksArray);
+      displayButtons();
     } else {
       const index = tasksArray.findIndex(el => +event.target.parentNode.id === el.id);
       tasksArray[index].isCompleted = false;
-      displayTasks();
+      displayTasks(tasksArray);
+      displayButtons();
     }
   }
 }
@@ -136,83 +130,34 @@ tasksList.onclick = deleteTask;
 
 // Filtering tasks
 
-const completedTasks = document.getElementById('filter__completed');
-
 completedTasks.onclick = () => {
   const filteredArray = tasksArray.filter(task => task.isCompleted === true);
   tasksList.innerHTML = null;
-  filteredArray.forEach(task => {
-    const div = document.createElement('div');
-    const btnDelete = document.createElement('button');
-    const btnComplete = document.createElement('button');
-    btnDelete.appendChild(document.createTextNode('delete'));
-    btnComplete.appendChild(document.createTextNode('complete'));
-
-    div.classList.add('border', 'rounded', 'd-flex', 'justify-content-between', 'p-2', 'mt-2');
-    btnComplete.classList.add('btn', 'btn-sm', 'btn-success');
-    btnDelete.classList.add('btn', 'btn-sm', 'btn-danger');
-    div.appendChild(document.createTextNode(task.title));
-    div.appendChild(btnComplete);
-    div.appendChild(btnDelete);
-    div.id = task.id;
-
-    tasksList.appendChild(div);
-  })
+  displayTasks(filteredArray);
+  displayButtons();
 }
-
-const activeTasks = document.getElementById('filter__active');
 
 activeTasks.onclick = () => {
   const filteredArray = tasksArray.filter(task => task.isCompleted === false);
   tasksList.innerHTML = null;
-  filteredArray.forEach(task => {
-    const div = document.createElement('div');
-    const btnDelete = document.createElement('button');
-    const btnComplete = document.createElement('button');
-    btnDelete.appendChild(document.createTextNode('delete'));
-    btnComplete.appendChild(document.createTextNode('complete'));
-
-    div.classList.add('border', 'rounded', 'd-flex', 'justify-content-between', 'p-2', 'mt-2');
-    btnComplete.classList.add('btn', 'btn-sm', 'btn-success');
-    btnDelete.classList.add('btn', 'btn-sm', 'btn-danger');
-    div.appendChild(document.createTextNode(task.title));
-    div.appendChild(btnComplete);
-    div.appendChild(btnDelete);
-    div.id = task.id;
-
-    tasksList.appendChild(div);
-  })
-
+  displayTasks(filteredArray);
+  displayButtons();
 }
-
-const allTasks = document.getElementById('filter__all');
 
 allTasks.onclick = () => {
   tasksList.innerHTML = null;
-  tasksArray.forEach(task => {
-    const div = document.createElement('div');
-    const btnDelete = document.createElement('button');
-    const btnComplete = document.createElement('button');
-    btnDelete.appendChild(document.createTextNode('delete'));
-    btnComplete.appendChild(document.createTextNode('complete'));
-
-    div.classList.add('border', 'rounded', 'd-flex', 'justify-content-between', 'p-2', 'mt-2');
-    btnComplete.classList.add('btn', 'btn-sm', 'btn-success');
-    btnDelete.classList.add('btn', 'btn-sm', 'btn-danger');
-    div.appendChild(document.createTextNode(task.title));
-    div.appendChild(btnComplete);
-    div.appendChild(btnDelete);
-    div.id = task.id;
-
-    tasksList.appendChild(div);
-  })
-
+  displayTasks(tasksArray);
+  displayButtons();
 }
 
 deleteCompleted.onclick = () => {
   const filteredArray = tasksArray.filter(task => task.isCompleted === true);
   filteredArray.forEach(task => {
+    const index = tasksArray.findIndex(el => el.id === task.id);
+    tasksArray.splice(index, 1);
     const taskToDelete = document.getElementById(task.id);
     tasksList.removeChild(taskToDelete);
   })
+  displayButtons();
+  deleteCompletedBtn();
 }
